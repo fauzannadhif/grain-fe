ca29e9a18885d9cd57068010247a2df93c644489
fauzannadhif <fauzannadhifm98@gmail.com>
2024-02-23 19:21:17 +0800

    add  cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import { useQuery } from "@apollo/client";
 import { getAllMenus } from "./queries";
+import ItemCard from "./ItemCard";
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import './App.css'
 
 const App = () => {
   const { loading, error, data } = useQuery(getAllMenus)
 
   return (
     <div className="App">
-      <h1>Hello</h1>
+      {data?.menus?.map(menu => 
+        <div key={menu.id}>
+          {menu.sections?.map(section =>
+            <section key={section.id}>
+              <Container style={{paddingBottom: 20, paddingTop: 20}}>
+                <div>
+                  <h4 className="font-link">
+                    {section.label}
+                  </h4>
+                  <h6 className="font-link">
+                    {section.description}
+                  </h6>
+                </div>
+                <Row>
+                  {section.items?.map(item =>
+                    <Col sm={2} key={item.id}>
+                        <ItemCard item={item} key={item.id}/>
+                    </Col>
+                  )}
+                </Row>
+              </Container>
+            </section>
+          )}
+        </div>
+      )}
     </div>
   )
 }
---
+++ src/ItemCard.js
@@ -0,0 +1,23 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+const ItemCard = ( { item } ) => {
+  return (
+    <Card className="shadow" style={{height: '55vh', backgroundColor: "#fcfcfa"}}>
+      <Card.Img style={{height: '30vh'}} variant="top" src={require('./pizza.jpg')} />
+      <Card.Body style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+        <div>
+        <Card.Title className="font-link">{item.label}</Card.Title>
+        <Card.Text className="font-link">
+          {item.description}
+        </Card.Text>
+        </div>
+        <div style={{verticalAlign: 'middle'}}>
+          <span className="font-link">${item.price}</span>
+          <Button variant="primary" className="float-end font-link">Add</Button>
+        </div>
+      </Card.Body>
+    </Card>
+  )
+}
+export default ItemCard;
